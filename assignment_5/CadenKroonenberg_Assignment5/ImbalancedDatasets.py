910b4ffe2fb21dfe648c13ab26af1d3c0abffacf
Caden Kroonenberg <cskroonenberg@gmail.com>
2021-10-25 17:29:03 -0500

    Complete lab & add output

--- assignment_5/CadenKroonenberg_Assignment5/ImbalancedDatasets.py
+++ assignment_5/CadenKroonenberg_Assignment5/ImbalancedDatasets.py
@@ -3,23 +3,17 @@
 
 import numpy as np
 from pandas import read_csv
-from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, recall_score, precision_score, balanced_accuracy_score
 from sklearn.neural_network import MLPClassifier
-from imblearn.over_sampling import RandomOverSampler 
+from imblearn.over_sampling import RandomOverSampler, SMOTE, ADASYN
+from imblearn.under_sampling import RandomUnderSampler, ClusterCentroids, TomekLinks
 
 def print_results(true, predicted):
     print('Confusion Matrix:')
     print(confusion_matrix(true, predicted))
     print('Accuracy Score:')
     print(accuracy_score(true, predicted))
-    print('Class Balanced Accuracy')
-    print(class_balanced_accuracy(true, predicted))
-    print('Balanced Accuracy')
-    print(balanced_accuracy(true, predicted))
-    print('Scikit Balanced Accuracy Score')
-    print(balanced_accuracy_score(true, predicted))
 
 def class_balanced_accuracy(true, predicted):
     # Calculate recall and precision
@@ -65,25 +59,143 @@ array = dataset.values
 X = array[:,0:4]
 y = array[:,4]
 
-# encode classes
-encoded_classes = LabelEncoder()
-encoded_classes.fit(y)
-encoded_vals = encoded_classes.transform(y)
-
 # split data into 2 folds for training and test
-X_trainFold1, X_testFold1, y_trainFold1, y_testFold1 = train_test_split(X, encoded_vals, test_size=0.50, random_state=1)
+X_trainFold1, X_testFold1, y_trainFold1, y_testFold1 = train_test_split(X, y, test_size=0.50, random_state=1)
 X_trainFold2 = X_testFold1
 X_testFold2 = X_trainFold1
 y_trainFold2 = y_testFold1
 y_testFold2 = y_trainFold1
 y_true = np.concatenate([y_testFold1, y_testFold2])
 
-# Neural Net
-nn_model = MLPClassifier(max_iter=1000) # Increase maximum interation count to resolve convergence issue
+# Original Imbalanced Data
+print('\nPART ONE - IMBALANCED DATA SET')
+nn_model = MLPClassifier(max_iter=1000) # Increase maximum iteration count to resolve convergence issue
 nn_model.fit(X_trainFold1, y_trainFold1)
 nn_pred_fold1 = nn_model.predict(X_testFold1)
 nn_model.fit(X_trainFold2, y_trainFold2)
 nn_pred_fold2 = nn_model.predict(X_testFold2)
 nn_y_pred = np.concatenate([nn_pred_fold1, nn_pred_fold2])
 print('\nNEURAL NET - Imbalanced Data Set')
+print_results(y_true, nn_y_pred)
+print('Class Balanced Accuracy:')
+print(class_balanced_accuracy(y_true, nn_y_pred))
+print('Balanced Accuracy:')
+print(balanced_accuracy(y_true, nn_y_pred))
+print('Scikit Balanced Accuracy Score:')
+print(balanced_accuracy_score(y_true, nn_y_pred))
+
+# Random Oversampling
+print('\nPART TWO - OVERSAMPLING')
+ros = RandomOverSampler(random_state=0)
+X_resampled, y_resampled = ros.fit_resample(X, y)
+X_trainFold1, X_testFold1, y_trainFold1, y_testFold1 = train_test_split(X_resampled, y_resampled, test_size=0.50, random_state=1)
+X_trainFold2 = X_testFold1
+X_testFold2 = X_trainFold1
+y_trainFold2 = y_testFold1
+y_testFold2 = y_trainFold1
+y_true = np.concatenate([y_testFold1, y_testFold2])
+
+nn_model = MLPClassifier(max_iter=1000) # Increase maximum iteration count to resolve convergence issue
+nn_model.fit(X_trainFold1, y_trainFold1)
+nn_pred_fold1 = nn_model.predict(X_testFold1)
+nn_model.fit(X_trainFold2, y_trainFold2)
+nn_pred_fold2 = nn_model.predict(X_testFold2)
+nn_y_pred = np.concatenate([nn_pred_fold1, nn_pred_fold2])
+print('\nNEURAL NET - Random Oversampling')
+print_results(y_true, nn_y_pred)
+
+# SMOTE
+X_resampled, y_resampled = SMOTE().fit_resample(X, y)
+X_trainFold1, X_testFold1, y_trainFold1, y_testFold1 = train_test_split(X_resampled, y_resampled, test_size=0.50, random_state=1)
+X_trainFold2 = X_testFold1
+X_testFold2 = X_trainFold1
+y_trainFold2 = y_testFold1
+y_testFold2 = y_trainFold1
+y_true = np.concatenate([y_testFold1, y_testFold2])
+
+nn_model = MLPClassifier(max_iter=1000) # Increase maximum iteration count to resolve convergence issue
+nn_model.fit(X_trainFold1, y_trainFold1)
+nn_pred_fold1 = nn_model.predict(X_testFold1)
+nn_model.fit(X_trainFold2, y_trainFold2)
+nn_pred_fold2 = nn_model.predict(X_testFold2)
+nn_y_pred = np.concatenate([nn_pred_fold1, nn_pred_fold2])
+print('\nNEURAL NET - SMOTE')
+print_results(y_true, nn_y_pred)
+
+# ADASYN
+ada = ADASYN(random_state=0, sampling_strategy='minority' )
+X_resampled, y_resampled = ada.fit_resample(X, y)
+X_trainFold1, X_testFold1, y_trainFold1, y_testFold1 = train_test_split(X_resampled, y_resampled, test_size=0.50, random_state=1)
+X_trainFold2 = X_testFold1
+X_testFold2 = X_trainFold1
+y_trainFold2 = y_testFold1
+y_testFold2 = y_trainFold1
+y_true = np.concatenate([y_testFold1, y_testFold2])
+
+nn_model = MLPClassifier(max_iter=1000) # Increase maximum iteration count to resolve convergence issue
+nn_model.fit(X_trainFold1, y_trainFold1)
+nn_pred_fold1 = nn_model.predict(X_testFold1)
+nn_model.fit(X_trainFold2, y_trainFold2)
+nn_pred_fold2 = nn_model.predict(X_testFold2)
+nn_y_pred = np.concatenate([nn_pred_fold1, nn_pred_fold2])
+print('\nNEURAL NET - ADASYN')
+print_results(y_true, nn_y_pred)
+
+# Random Under Sampling
+print('\nPART THREE - UNDERSAMPLING')
+rus = RandomUnderSampler(random_state=0)
+X_resampled, y_resampled = rus.fit_resample(X, y)
+X_trainFold1, X_testFold1, y_trainFold1, y_testFold1 = train_test_split(X_resampled, y_resampled, test_size=0.50, random_state=1)
+X_trainFold2 = X_testFold1
+X_testFold2 = X_trainFold1
+y_trainFold2 = y_testFold1
+y_testFold2 = y_trainFold1
+y_true = np.concatenate([y_testFold1, y_testFold2])
+
+nn_model = MLPClassifier(max_iter=1000) # Increase maximum iteration count to resolve convergence issue
+nn_model.fit(X_trainFold1, y_trainFold1)
+nn_pred_fold1 = nn_model.predict(X_testFold1)
+nn_model.fit(X_trainFold2, y_trainFold2)
+nn_pred_fold2 = nn_model.predict(X_testFold2)
+nn_y_pred = np.concatenate([nn_pred_fold1, nn_pred_fold2])
+print('\nNEURAL NET - Random Undersampling')
+print_results(y_true, nn_y_pred)
+
+# Cluster
+cc = ClusterCentroids(random_state=0)
+X_resampled, y_resampled = cc.fit_resample(X, y)
+X_trainFold1, X_testFold1, y_trainFold1, y_testFold1 = train_test_split(X_resampled, y_resampled, test_size=0.50, random_state=1)
+X_trainFold2 = X_testFold1
+X_testFold2 = X_trainFold1
+y_trainFold2 = y_testFold1
+y_testFold2 = y_trainFold1
+y_true = np.concatenate([y_testFold1, y_testFold2])
+
+nn_model = MLPClassifier(max_iter=1000) # Increase maximum iteration count to resolve convergence issue
+nn_model.fit(X_trainFold1, y_trainFold1)
+nn_pred_fold1 = nn_model.predict(X_testFold1)
+nn_model.fit(X_trainFold2, y_trainFold2)
+nn_pred_fold2 = nn_model.predict(X_testFold2)
+nn_y_pred = np.concatenate([nn_pred_fold1, nn_pred_fold2])
+print('\nNEURAL NET - Cluster')
+print_results(y_true, nn_y_pred)
+
+# Tomek Links
+# TODO -- Did this even work right??
+tl = TomekLinks()
+X_resampled, y_resampled = tl.fit_resample(X, y)
+X_trainFold1, X_testFold1, y_trainFold1, y_testFold1 = train_test_split(X_resampled, y_resampled, test_size=0.50, random_state=1)
+X_trainFold2 = X_testFold1
+X_testFold2 = X_trainFold1
+y_trainFold2 = y_testFold1
+y_testFold2 = y_trainFold1
+y_true = np.concatenate([y_testFold1, y_testFold2])
+
+nn_model = MLPClassifier(max_iter=1000) # Increase maximum iteration count to resolve convergence issue
+nn_model.fit(X_trainFold1, y_trainFold1)
+nn_pred_fold1 = nn_model.predict(X_testFold1)
+nn_model.fit(X_trainFold2, y_trainFold2)
+nn_pred_fold2 = nn_model.predict(X_testFold2)
+nn_y_pred = np.concatenate([nn_pred_fold1, nn_pred_fold2])
+print('\nNEURAL NET - Tomek Links')
 print_results(y_true, nn_y_pred)
